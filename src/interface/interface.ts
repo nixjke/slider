5a300cff5aae0342c3655432db1c50756cbc75a0
SosiskaPish <aziz.10.03@mail.ru>
2021-10-21 15:59:43 +0300

    add interface

---
+++ src/interface/interface.ts
@@ -0,0 +1,8 @@
+interface ModelState {
+  min?: number
+  max?: number
+  step?: number
+  values?: number[]
+}
+
+export default ModelState
--- test/cat.unit.test.ts
+++ test/cat.unit.test.ts
@@ -1,32 +1,33 @@
-import { Cat } from '../src/cat.module';
-import { suite, test } from '@testdeck/mocha';
-import * as _chai from 'chai';
-import { expect } from 'chai';
+import { Cat } from '../src/cat.module'
+import { suite, test } from '@testdeck/mocha'
+import * as _chai from 'chai'
+import { expect } from 'chai'
 
-_chai.should();
-_chai.expect;
+_chai.should()
+_chai.expect
 
-@suite class CatModuleTest {
-  private SUT: Cat;
-  private name: string;
-  private color: string;
+@suite
+class CatModuleTest {
+  private SUT: Cat
+  private name: string
+  private color: string
 
   before() {
-    this.name = 'Tom';
-    this.color = 'black';
-    this.SUT = new Cat(this.name, this.color);
+    this.name = 'Tom'
+    this.color = 'black'
+    this.SUT = new Cat(this.name, this.color)
   }
 
-  @test 'Cat is created' () {
-    this.SUT.name.should.to.not.be.undefined.and.have.property('name').equal('Tom');
+  @test 'Cat is created'() {
+    this.SUT.name.should.to.not.be.undefined.and.have.property('name').equal('Tom')
   }
 
-  @test 'Cat move 10m' () {
-    let catMove = this.SUT.move(10);
-    expect(catMove).to.be.equal('Tom moved 10m.');
+  @test 'Cat move 10m'() {
+    let catMove = this.SUT.move(10)
+    expect(catMove).to.be.equal('Tom moved 10m.')
   }
 
-  @test 'Cat say meow' () {
-    expect(this.SUT.say()).to.be.equal('Cat Tom says meow');
+  @test 'Cat say meow'() {
+    expect(this.SUT.say()).to.be.equal('Cat Tom says meow')
   }
 }
