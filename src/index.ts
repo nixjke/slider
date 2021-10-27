2aff7253cdb86fe4ccb4a1dc884f1043678fe032
SosiskaPish <aziz.10.03@mail.ru>
2021-10-27 18:52:46 +0300

    made variables to constants

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import './View/scss/index.scss'
-import { Controller } from './Controller/Controller'
+import './plugin/View/scss/index.scss'
+import { Controller } from './plugin/Controller/Controller'
 const slider = document.getElementById("slider") as HTMLElement;
 
 const controller = new Controller(slider);
