b28b7b2ca9afb7a86946d4b457e1425fd124433d
SosiskaPish <aziz.10.03@mail.ru>
2021-10-29 14:00:08 +0300

    Decomposition of the Abstract Factory

--- src/plugin/View/AbstractFactory/Application.ts
+++ src/plugin/View/AbstractFactory/Application.ts
@@ -1,267 +1,44 @@
-import { ITemp, IVisualModel } from '../../../utils/interface'
+import { ITemp, IVisualModel, IOnlyBoolean, IOnlyString } from '../../../utils/interface'
 import Observer from '../../Observer/Observer'
 import constants from '../../../utils/constants'
 
 /**
  * Bar
  */
-
-interface Bar {
-  paint({ tempPxValue, tempPxValues, tempValue, tempTarget }: ITemp): void
-  init(anchor: HTMLElement): void
-}
-
-class Bar implements Bar {
-  public init(anchor: HTMLElement) {
-    const barTemplate = `<div class="slider__bar"></div>`
-    const slider = anchor.querySelector('.slider') as HTMLElement
-    slider.insertAdjacentHTML('beforeend', barTemplate)
-  }
-}
-
-class SingleHorizontalBar extends Bar implements Bar {
-  public paint({ tempPxValue, tempTarget }: ITemp) {
-    const bar = tempTarget.parentElement && (tempTarget.parentElement.querySelector('.slider__bar') as HTMLElement)
-
-    bar && (bar.style.width = tempPxValue + 10 + 'px')
-  }
-}
-
-class SingleVerticalBar extends Bar implements Bar {
-  public paint({ tempPxValue, tempTarget }: ITemp) {
-    const bar = tempTarget.parentElement && (tempTarget.parentElement.querySelector('.slider__bar') as HTMLElement)
-
-    bar && (bar.style.height = tempPxValue + 10 + 'px')
-  }
-}
-
-class IntervalHorizontalBar extends Bar implements Bar {
-  public paint({ tempPxValues, tempTarget }: ITemp) {
-    const bar = tempTarget.parentElement && (tempTarget.parentElement.querySelector('.slider__bar') as HTMLElement)
-
-    bar && (bar.style.left = tempPxValues[0] + 'px')
-    bar && (bar.style.width = tempPxValues[1] - tempPxValues[0] + 10 + 'px')
-  }
-}
-
-class IntervalVerticalBar extends Bar implements Bar {
-  public paint({ tempPxValues, tempTarget }: ITemp) {
-    const bar = tempTarget.parentElement && (tempTarget.parentElement.querySelector('.slider__bar') as HTMLElement)
-
-    bar && (bar.style.bottom = tempPxValues[0] + 'px')
-    bar && (bar.style.height = tempPxValues[1] - tempPxValues[0] + 10 + 'px')
-  }
-}
+import { Bar, SingleHorizontalBar, SingleVerticalBar, IntervalHorizontalBar, IntervalVerticalBar } from './UIs/Bar/Bar'
 
 /**
  * Tip
  */
-
-class Tip {
-  public paint({ tempTarget, tempValue }: ITemp) {
-    const tip = tempTarget.querySelector('.slider__tip') as HTMLElement
-    tip.setAttribute('data-value', `${tempValue}`)
-  }
-
-  public init(handler: HTMLElement) {
-    const tipTemplate = `<div class="slider__tip"><div class="slider__tongue"></div></div>`
-    handler.insertAdjacentHTML('beforeend', tipTemplate)
-  }
-}
+import { Tip } from './UIs/Tip/Tip'
 
 /**
  * Handler
  */
-
-interface Handler {
-  paint({ tempTarget, tempPxValue }: ITemp): void
-  init(anchor: HTMLElement): void
-  append(component: Tip): void
-}
-
-class Handler implements Handler {
-  protected anchor!: HTMLElement
-
-  public init(anchor: HTMLElement) {
-    this.anchor = anchor
-    const handlerTemplate = `<div class="slider__handler"></div>`
-    const slider = anchor.querySelector('.slider') as HTMLElement
-    slider.insertAdjacentHTML('beforeend', handlerTemplate)
-  }
-
-  public append(component: Tip) {
-    const handlers = this.anchor.querySelectorAll('.slider__handler')
-
-    for (const handler of Array.from(handlers)) {
-      component.init(handler as HTMLElement)
-    }
-  }
-}
-
-class SingleHorizontalHandler extends Handler implements Handler {
-  public paint({ tempTarget, tempPxValue }: ITemp) {
-    tempTarget.style.left = tempPxValue + 'px'
-  }
-}
-
-class SingleVerticalHandler extends Handler implements Handler {
-  public paint({ tempTarget, tempPxValue }: ITemp) {
-    tempTarget.style.bottom = tempPxValue + 'px'
-  }
-}
-
-class IntervalHorizontalHandler extends Handler implements Handler {
-  public init(anchor: HTMLElement) {
-    this.anchor = anchor
-    const handlerTemplate = `<div class="slider__handler"></div>`
-    const slider = anchor.querySelector('.slider') as HTMLElement
-    slider.insertAdjacentHTML('beforeend', handlerTemplate)
-    slider.insertAdjacentHTML('beforeend', handlerTemplate)
-  }
-
-  public paint({ tempTarget, tempPxValue }: ITemp) {
-    tempTarget.style.left = tempPxValue + 'px'
-  }
-}
-
-class IntervalVerticalHandler extends Handler implements Handler {
-  public init(anchor: HTMLElement) {
-    this.anchor = anchor
-    const handlerTemplate = `<div class="slider__handler"></div>`
-    const slider = anchor.querySelector('.slider') as HTMLElement
-    slider.insertAdjacentHTML('beforeend', handlerTemplate)
-    slider.insertAdjacentHTML('beforeend', handlerTemplate)
-  }
-
-  public paint({ tempTarget, tempPxValue }: ITemp) {
-    tempTarget.style.bottom = tempPxValue + 'px'
-  }
-}
+import {
+  Handler,
+  SingleHorizontalHandler,
+  SingleVerticalHandler,
+  IntervalHorizontalHandler,
+  IntervalVerticalHandler,
+} from './UIs/Handler/Handler'
 
 /**
  * Template
  */
-
-interface Template {
-  init({ skin, direction }: IVisualModel, anchor: HTMLElement): void
-  append(component: Handler | Bar | Tip, anchor: HTMLElement): void
-}
-
-class Template implements Template {
-  public init({ skin, direction }: IVisualModel, anchor: HTMLElement) {
-    const sliderTemplate = `
-      <div class="wrapper-slider wrapper-slider--${direction}">
-        <div class="slider slider--${direction} slider--${skin}"></div>
-      </div>
-    `
-
-    anchor.insertAdjacentHTML('afterbegin', sliderTemplate)
-  }
-
-  public append(component: Handler | Bar, anchor: HTMLElement) {
-    component.init(anchor)
-  }
-}
+import { Template } from './UIs/Template/Template'
 
 /**
  * Guifactory
  */
 
-interface GUIFactory {
-  createBar(): Bar
-  createTip(): Tip
-  createHandler(): Handler
-  createTemplate(): Template
-}
-
-/**
- * Factories
- */
-class SingleHorizontalFactory implements GUIFactory {
-  public createBar(): Bar {
-    return new SingleHorizontalBar()
-  }
-
-  public createTip(): Tip {
-    return new Tip()
-  }
-
-  public createHandler(): Handler {
-    return new SingleHorizontalHandler()
-  }
-
-  public createTemplate(): Template {
-    return new Template()
-  }
-}
-
-class SingleVerticalFactory implements GUIFactory {
-  public createBar(): Bar {
-    return new SingleVerticalBar()
-  }
-
-  public createTip(): Tip {
-    return new Tip()
-  }
-
-  public createHandler(): Handler {
-    return new SingleVerticalHandler()
-  }
-
-  public createTemplate(): Template {
-    return new Template()
-  }
-}
-
-class IntervalHorizontalFactory implements GUIFactory {
-  public createBar(): Bar {
-    return new IntervalHorizontalBar()
-  }
-
-  public createTip(): Tip {
-    return new Tip()
-  }
-
-  public createHandler(): Handler {
-    return new IntervalHorizontalHandler()
-  }
-
-  public createTemplate(): Template {
-    return new Template()
-  }
-}
-
-class IntervalVerticalFactory implements GUIFactory {
-  public createBar(): Bar {
-    return new IntervalVerticalBar()
-  }
-
-  public createTip(): Tip {
-    return new Tip()
-  }
-
-  public createHandler(): Handler {
-    return new IntervalVerticalHandler()
-  }
-
-  public createTemplate(): Template {
-    return new Template()
-  }
-}
-
-// TODO Вынести интерфейсы в helpers
-
-interface IOnlyBoolean {
-  [key: string]: boolean
-}
-
-interface IOnlyString {
-  [key: string]: string
-}
-
-interface IOnlyHTMLElements {
-  [key: string]: HTMLElement
-}
+import {
+  GUIFactory,
+  SingleHorizontalFactory,
+  SingleVerticalFactory,
+  IntervalHorizontalFactory,
+  IntervalVerticalFactory,
+} from './Factories/Factories'
 
 /**
  * Application
@@ -314,7 +91,6 @@ class Application extends Observer {
 
     for (const UI of UIs) {
       if (UI === 'factory' || UI === 'template' || UI === 'events' || UI === 'anchor') continue
-
       ;(this as any)[UI].paint(state)
     }
   }
