fbd633ef56a2de4448e866e191c5965e55ab6494
SosiskaPish <aziz.10.03@mail.ru>
2021-10-29 14:23:14 +0300

    restart

--- src/plugin/View/View.ts
+++
@@ -1,7 +0,0 @@
-import Observer from '../Observer/Observer'
-import constants from '../../utils/constants'
-import { IVisualModel } from '../../utils/interface'
-
-class View extends Observer {}
-
-export { View };
