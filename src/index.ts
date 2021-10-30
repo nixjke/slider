f53e38e22849c63e51b915b1be58ca9ec28f51dc
SosiskaPish <aziz.10.03@mail.ru>
2021-10-30 12:28:35 +0300

    Constructed architecture

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import './plugin/View/scss/index.scss'
-import { Controller } from './plugin/Controller/Controller'
-import { ApplicationConfigurator } from './plugin/View/AbstractFactory/Application'
-
-const slider = document.getElementById('slider') as HTMLElement
-const controller = new Controller(slider)
-
-console.log(slider)
-console.log(controller)
