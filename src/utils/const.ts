4022bbd140de9a2da802ffe8a25040de2de65723
SosiskaPish <aziz.10.03@mail.ru>
2021-11-29 12:24:15 +0300

    deleted slider

--- src/utils/const.ts
+++
@@ -1,6 +0,0 @@
-const constants = {
-  DIRECTION_VERTICAL: 'vertical',
-  TYPE_DOUBLE: 'double',
-}
-
-export { constants }
--- src/utils/interface.ts
+++
@@ -1,7 +0,0 @@
-interface IState {
-  [key: string]: number | number[]
-}
-interface IOnlyNumbers {
-  [key: string]: number
-}
-export { IState, IOnlyNumbers }
