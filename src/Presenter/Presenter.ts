ee9b8205432130fbef10f413202667a40726ad3b
SosiskaPish <aziz.10.03@mail.ru>
2021-12-16 14:09:28 +0300

    getSplitModelState

--- src/Presenter/Presenter.ts
+++ src/Presenter/Presenter.ts
@@ -11,7 +11,20 @@ class Presenter extends Observer {
   constructor(anchor: HTMLElement, state: IModelState) {
     super()
     this.anchor = anchor
-    this.model = new Model(state)
+    this.model = new Model(this.getSplitModelState(state))
     this.view = new View(state, anchor)
+  }
+
+  private getSplitModelState(state: any): IModelState {
+    const { values, range, scale, tip, step, orientation } = state
+
+    return {
+      values,
+      range,
+      scale,
+      tip,
+      step,
+      orientation,
+    }
   }
 }
