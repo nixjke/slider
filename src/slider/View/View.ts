7ac3971cd6cec8fe97b85a18d8f74ee59f2000a6
SosiskaPish <aziz.10.03@mail.ru>
2022-02-02 18:17:16 +0300

    init View

---
+++ src/slider/View/View.ts
@@ -0,0 +1,13 @@
+import Observer from '../Observer/Observer'
+import { ModelState } from '../utils/interfaces/Model'
+
+class View extends Observer {
+  private modelState: ModelState
+  private domParent: HTMLElement
+
+  constructor(modelState: ModelState, domParent: HTMLElement) {
+    super()
+    this.modelState = modelState
+    this.domParent = domParent
+  }
+}
