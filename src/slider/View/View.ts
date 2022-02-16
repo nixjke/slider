27d204a0ea4c6ad6388b184ddbee947c6e3f02fc
SosiskaPish <aziz.10.03@mail.ru>
2022-02-16 16:40:37 +0300

    init View state

--- src/slider/View/View.ts
+++ src/slider/View/View.ts
@@ -3,18 +3,31 @@ import { BarProps } from '../utils/interfaces/Components/IBar'
 import { ModelState } from '../utils/interfaces/Model'
 
 import Bar from './components/bar/Bar'
+import Ruler from './components/ruler/Ruler'
+import Toggle from './components/toggle/Toggle'
 
 class View extends Observer {
   private modelState: ModelState
   private domParent: HTMLElement
+  private slider!: HTMLElement
+
+  private isVertical: boolean
+  private isRange: boolean
+
   private bar!: Bar
+  private ruler!: Ruler | null
+  private toggles!: Toggle[]
+  private activeToggle!: Toggle
+  private activeToggleIndex!: number
 
   constructor(modelState: ModelState, domParent: HTMLElement) {
     super()
     this.modelState = modelState
     this.domParent = domParent
+    const { currentValues, orientation } = this.modelState
+    this.isVertical = orientation === 'vertical'
+    this.isRange = currentValues.hasOwnProperty('max')
     this.initViewComoponents()
-    console.log(this.bar)
   }
 
   private initViewComoponents() {
