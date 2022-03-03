0a28acba8aa66491bf5f6eb12ecccb041a6c9c74
SosiskaPish <aziz.10.03@mail.ru>
2022-03-03 21:41:43 +0300

    Tests for Model

--- test/Model/Model.test.ts
+++ test/Model/Model.test.ts
@@ -1,6 +1,7 @@
 import Model from '../../src/slider/Model/Model'
 import { ModelState } from '../../src/slider/utils/interfaces/Model'
 import defaultState from '../../src/slider/utils/defaultState'
+import { mode } from '../../webpack.config'
 
 let modelState: ModelState
 let model: Model
@@ -48,5 +49,30 @@ describe('Model', () => {
     model.updateState(newSliderState)
     expect(model.getState()).toEqual(newSliderState)
   })
-  
+
+  it('cuurentValues.max cannot be < range.min', () => {
+    const newSliderState: ModelState = {
+      ...modelState,
+      currentValues: { min: 0, max: -1 },
+      range: { min: 0, max: 100 },
+    }
+    model.updateState(newSliderState)
+    expect(model.getState()).toEqual(newSliderState)
+  })
+
+  it('currentValues cannot be NaN', () => {
+    const newSliderState: ModelState = {
+      ...modelState,
+      currentValues: { min: NaN, max: 100 },
+    }
+    model.updateState(newSliderState)
+    expect(model.getState()).toEqual(newSliderState)
+
+    const stateWithCurrentValueMaxNaN: ModelState = {
+      ...modelState,
+      currentValues: { min: 1, max: NaN },
+    }
+    model.updateState(stateWithCurrentValueMaxNaN)
+    expect(model.getState()).toEqual(stateWithCurrentValueMaxNaN)
+  })
 })
