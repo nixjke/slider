9c3695b7ebb98fa6664fc643ed98096a8c1ac976
SosiskaPish <aziz.10.03@mail.ru>
2022-03-11 10:00:46 +0300

    Tests for View

--- test/View/View.test.ts
+++ test/View/View.test.ts
@@ -83,4 +83,70 @@ describe('View', () => {
     expect(document.querySelectorAll(`.${sliderClassNames.bar}`).length).toEqual(1)
     expect(document.querySelectorAll(`.${sliderClassNames.thumb}`).length).toEqual(0)
   })
+
+  it('Creates a vertical slider', () => {
+    const rangeSliderOptions: ModelState = {
+      ...modelState,
+      orientation: 'vertical',
+      ruler: true,
+    }
+    const view = new View(rangeSliderOptions, document.querySelector('.js-default-slider') as HTMLElement)
+    view.render()
+    const isBarHaveVerticalClass = (
+      document.querySelector(`.${sliderClassNames.bar}`) as HTMLElement
+    ).classList.contains(`${sliderClassNames.barVertical}`)
+    const isToggleHaveVerticalClass = (
+      document.querySelector(`.${sliderClassNames.toggle}`) as HTMLElement
+    ).classList.contains(`${sliderClassNames.toggleVertical}`)
+    const isRulerHaveVerticalClass = (
+      document.querySelector(`.${sliderClassNames.ruler}`) as HTMLElement
+    ).classList.contains(`${sliderClassNames.rulerVertical}`)
+    expect(document.querySelectorAll(`.${sliderClassNames.slider}`).length).toEqual(1)
+    expect(document.querySelectorAll(`.${sliderClassNames.wrap}`).length).toEqual(1)
+    expect(document.querySelectorAll(`.${sliderClassNames.scale}`).length).toEqual(1)
+    expect(document.querySelectorAll(`.${sliderClassNames.bar}`).length).toEqual(1)
+    expect(isBarHaveVerticalClass).toBeTruthy()
+    expect(isToggleHaveVerticalClass).toBeTruthy()
+    expect(isRulerHaveVerticalClass).toBeTruthy()
+  })
+
+  it('Notifies you when you click on the bar', () => {
+    const rangeSliderState = { ...modelState }
+    const view = new View(rangeSliderState, document.querySelector('.js-default-slider') as HTMLElement)
+    const spy = jest.spyOn(view, 'notify')
+    view.render()
+    const bar = document.querySelector(`.${sliderClassNames.bar}`) as HTMLElement
+    bar.click()
+    expect(spy).toBeCalled()
+  })
+
+  it('Handles bar clicks with currentValues.min and currentValues.max', () => {
+    const rangeSliderState = { ...modelState, currentValues: { min: 0, max: 100 } }
+    const view = new View(rangeSliderState, document.querySelector('.js-default-slider') as HTMLElement)
+    const spy = jest.spyOn(view, 'notify')
+    view.render()
+    const bar = document.querySelector(`.${sliderClassNames.bar}`) as HTMLElement
+    bar.click()
+    expect(spy).toBeCalled()
+  })
+
+  it('Handles bar click if currentValue.min === currentValue.max', () => {
+    const rangeSliderState = { ...modelState, currentValues: { min: 15, max: 15 } }
+    const view = new View(rangeSliderState, document.querySelector('.js-default-slider') as HTMLElement)
+    const spy = jest.spyOn(view, 'notify')
+    view.render()
+    const bar = document.querySelector(`.${sliderClassNames.bar}`) as HTMLElement
+    bar.click()
+    expect(spy).toBeCalled()
+  })
+
+  it('Handles the click on thumb', () => {
+    const rangeSliderState = { ...modelState }
+    const view = new View(rangeSliderState, document.querySelector('.js-default-slider') as HTMLElement)
+    view.render()
+    const toggle = document.querySelector(`.${sliderClassNames.toggle}`) as HTMLElement
+    const thumb = document.querySelector(`.${sliderClassNames.thumb}`) as HTMLElement
+    thumb.dispatchEvent(new MouseEvent('mousedown', { bubbles: true }))
+    expect(toggle.classList.contains(`${sliderClassNames.toggleActive}`)).toBeTruthy()
+  })
 })
